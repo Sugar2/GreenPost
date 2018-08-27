c2aa049ea504717fb7ac2c1f27ff79f9559424f9
Sugar2 <vitaliytkachuck@gmail.com>
2018-08-27 21:12:34 +0300

    Logistics-couriers

---
+++ greenpost/src/app/components/workspaces/logistics-courier/card/card.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+import { TransactionModel } from '../../../../models';
+import { MatTableDataSource } from '@angular/material';
+
+@Component({
+  selector: 'logistics-courier-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+
+export class LogisticsCourierCardComponent {
+    dataSource = new MatTableDataSource<TransactionModel>(data);
+}
+
+const data: TransactionModel[] = [
+    { id: 1, from: 'Mayakovskoho 18', to: 'Warehouse 1', status: 'done' },
+    { id: 2, from: 'Warehouse 1', to: 'Warehouse 3', status: 'active' },
+    { id: 3, from: 'Warehouse 3', to: 'Lavrukhina 7/1', status: 'waiting' }
+]
---
+++ greenpost/src/app/components/workspaces/logistics-courier/section/section.component.ts
@@ -0,0 +1,52 @@
+import {Component} from '@angular/core';
+import {OrderModel} from '../../../../models';
+import { MatTableDataSource, MatBottomSheet } from '@angular/material';
+import { Router } from '@angular/router';
+import { FileLoadComponent } from '../../../widgets/file-load/file-load.component';
+import {SelectionModel} from '@angular/cdk/collections';
+
+@Component({
+    selector: 'logistics-courier-section',
+    templateUrl: './section.component.html',
+    styleUrls: ['./section.component.scss']
+})
+
+export class LogisticsCourierSectionComponent {
+    constructor(private router: Router, private bottomSheet: MatBottomSheet) { }
+    dataSource = new MatTableDataSource<OrderModel>(data);
+
+    openCard(rowId: number): void {
+        this.router.navigate(['logistics-couriers', 'card'])
+    }
+
+    openBottomSheet(): void {
+        this.bottomSheet.open(FileLoadComponent);
+    }
+
+    displayedColumns: string[] = ['select', 'from', 'to', 'reciever', 'status'];
+    selection = new SelectionModel<OrderModel>(true, []);
+
+    /** Whether the number of selected elements matches the total number of rows. */
+    isAllSelected() {
+        const numSelected = this.selection.selected.length;
+        const numRows = this.dataSource.data.length;
+        return numSelected === numRows;
+    }
+
+    /** Selects all rows if they are not all selected; otherwise clear selection. */
+    masterToggle() {
+        this.isAllSelected() ?
+            this.selection.clear() :
+            this.dataSource.data.forEach(row => this.selection.select(row));
+    }
+
+
+}
+
+const data: OrderModel[] = [
+    { id: 1, from: 'Mayakovskoho 18', to: 'Lavrukhina 7/1', reciever: 'Igor Ivanov', status: 'Active' },
+    { id: 2, from: 'Mayakovskoho 18', to: 'Lavrukhina 7/1', reciever: 'Igor Ivanov', status: 'Active' },
+    { id: 3, from: 'Mayakovskoho 18', to: 'Lavrukhina 7/1', reciever: 'Igor Ivanov', status: 'Active' },
+    { id: 4, from: 'Mayakovskoho 18', to: 'Lavrukhina 7/1', reciever: 'Igor Ivanov', status: 'Active' },
+    { id: 5, from: 'Mayakovskoho 18', to: 'Lavrukhina 7/1', reciever: 'Igor Ivanov', status: 'Active' }
+]
