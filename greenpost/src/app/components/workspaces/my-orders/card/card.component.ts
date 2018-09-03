14913d43f4177d162f9b548c27ac292d0d610a31
Sugar2 <vitaliytkachuck@gmail.com>
2018-09-03 14:59:54 +0300

    Add phone input into details

--- greenpost/src/app/components/workspaces/my-orders/card/card.component.ts
+++ greenpost/src/app/components/workspaces/my-orders/card/card.component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
 import { TransactionModel } from '../../../../models';
-import { MatTableDataSource } from '@angular/material';
+import {MatChipInputEvent, MatTableDataSource} from '@angular/material';
+import {COMMA, ENTER} from "@angular/cdk/keycodes";
+import {PhoneMyOrdersCard} from "../../..";
 export interface TypesOfDelivery {
     value: string;
     viewValue: string;
+}
+export interface PhoneMyOrdersCard {
+    name: string;
 }
 @Component({
   selector: 'my-orders-card',
@@ -17,6 +22,39 @@ export class MyOrdersCardComponent {
         {value: 'standart-0', viewValue: 'Стандарт'},
         {value: 'express-1', viewValue: 'Экспресс'}
     ];
+
+
+    visible = true;
+    selectable = true;
+    removable = true;
+    addOnBlur = true;
+    readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+    phones: PhoneMyOrdersCard[] = [
+        {name: '+380990664449'},
+    ];
+
+    add(event: MatChipInputEvent): void {
+        const input = event.input;
+        const value = event.value;
+
+        // Add our fruit
+        if ((value || '').trim()) {
+            this.phones.push({name: value.trim()});
+        }
+
+        // Reset the input value
+        if (input) {
+            input.value = '';
+        }
+    }
+
+    remove(phone: PhoneMyOrdersCard): void {
+        const index = this.phones.indexOf(phone);
+
+        if (index >= 0) {
+            this.phones.splice(index, 1);
+        }
+    }
 }
 
 const data: TransactionModel[] = [
