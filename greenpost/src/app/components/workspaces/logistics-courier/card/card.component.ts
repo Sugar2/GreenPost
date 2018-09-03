a70f9bcfdceea8ffe84b55e31c47a783d56f9f53
Sugar2 <vitaliytkachuck@gmail.com>
2018-09-03 09:47:38 +0300

    edits

--- greenpost/src/app/components/workspaces/logistics-courier/card/card.component.ts
+++ greenpost/src/app/components/workspaces/logistics-courier/card/card.component.ts
@@ -1,7 +1,11 @@
 import {Component} from '@angular/core';
 import { TransactionModel } from '../../../../models';
 import { MatTableDataSource } from '@angular/material';
-
+import {TypesOfDelivery} from "../../..";
+export interface TypesOfDeliveryCourier {
+    value: string;
+    viewValue: string;
+}
 @Component({
   selector: 'logistics-courier-card',
   templateUrl: './card.component.html',
@@ -10,10 +14,14 @@ import { MatTableDataSource } from '@angular/material';
 
 export class LogisticsCourierCardComponent {
     dataSource = new MatTableDataSource<TransactionModel>(data);
+    typesOfDelivery: TypesOfDeliveryCourier[] = [
+        {value: 'standart-0', viewValue: 'Стандарт'},
+        {value: 'express-1', viewValue: 'Экспресс'}
+    ];
 }
 
 const data: TransactionModel[] = [
-    { id: 1, from: 'Mayakovskoho 18', to: 'Warehouse 1', status: 'done' },
-    { id: 2, from: 'Warehouse 1', to: 'Warehouse 3', status: 'active' },
-    { id: 3, from: 'Warehouse 3', to: 'Lavrukhina 7/1', status: 'waiting' }
+    { id: 1, from: 'Маяковского 18', to: 'Склад 1', status: 'сделан' },
+    { id: 2, from: 'Склад 1', to: 'Склад 3', status: 'активен' },
+    { id: 3, from: 'Склад 3', to: 'Лаврухина 7/1', status: 'в ожидании' }
 ]
