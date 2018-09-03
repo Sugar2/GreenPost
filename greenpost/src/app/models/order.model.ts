f0556328a6781686faabc990a06f15a4cf0563b8
Sugar2 <vitaliytkachuck@gmail.com>
2018-09-03 10:10:54 +0300

    aded price and status col

--- greenpost/src/app/models/order.model.ts
+++ greenpost/src/app/models/order.model.ts
@@ -3,5 +3,7 @@ export class OrderModel {
     public reciever: string;
     public from: string;
     public to: string;
+    public price: string;
     public status: string;
+    public typeOfOrder: string;
 }
