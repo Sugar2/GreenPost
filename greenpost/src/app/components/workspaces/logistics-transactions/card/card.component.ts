47e516222d3986e93d1d38f40634d3284db8f5fe
Sugar2 <vitaliytkachuck@gmail.com>
2018-09-14 22:29:27 +0300

    Card fixed

--- greenpost/src/app/components/workspaces/logistics-transactions/card/card.component.ts
+++ greenpost/src/app/components/workspaces/logistics-transactions/card/card.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import { TransactionModel } from '../../../../models';
-import { MatTableDataSource } from '@angular/material';
+import {MatChipInputEvent, MatTableDataSource} from '@angular/material';
+import {COMMA, ENTER} from "@angular/cdk/keycodes";
+import {PhoneLogTransCard} from "../../..";
 export interface TypesOfDeliveryTrans {
     value: string;
     viewValue: string;
@@ -18,6 +20,39 @@ export class LogisticsTransactionsCardComponent {
         {value: 'standart-0', viewValue: 'Стандарт'},
         {value: 'express-1', viewValue: 'Экспресс'}
     ];
+
+
+    visible = true;
+    selectable = true;
+    removable = true;
+    addOnBlur = true;
+    readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+    phones: PhoneLogTransCard[] = [
+        {name: '+380990664449'},
+    ];
+
+    add(event: MatChipInputEvent): void {
+        const input = event.input;
+        const value = event.value;
+
+        // Add our fruit
+        if ((value || '').trim()) {
+            this.phones.push({name: value.trim()});
+        }
+
+        // Reset the input value
+        if (input) {
+            input.value = '';
+        }
+    }
+
+    remove(phone: PhoneLogTransCard): void {
+        const index = this.phones.indexOf(phone);
+
+        if (index >= 0) {
+            this.phones.splice(index, 1);
+        }
+    }
 }
 
 const data: TransactionModel[] = [
