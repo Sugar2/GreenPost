8f3386a541e9e558e22172a17aa59f986f8820f0
Sugar2 <vitaliytkachuck@gmail.com>
2018-09-14 22:37:03 +0300

    Add takes.model

--- greenpost/src/app/models/index.ts
+++ greenpost/src/app/models/index.ts
@@ -2,4 +2,5 @@ export * from './order.model';
 export * from './transaction.model';
 export * from './user.model';
 export * from './user.role.model';
-export * from './map.marker.model';
+export * from './map-marker.model';
+export * from './takes.model';
